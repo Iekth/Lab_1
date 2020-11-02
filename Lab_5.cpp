9f8a0106053c2933b9e086f5b64bf3c4008f9444
Iekth <71468179+Iekth@users.noreply.github.com>
2020-11-02 11:50:53 +0300

    Add files via upload

---
+++ Lab_5.cpp
@@ -0,0 +1,32 @@
+﻿// Lab_5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+#include<stdio.h>
+#include<conio.h>
+
+
+
+int main() {
+	int sum = 0;
+
+
+
+	int arr[2][3] = { 5,5,5,
+					  5,5,5 };
+
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 3; j++)
+
+
+
+
+		{
+		
+
+	if (arr[i][j] % 2 != 0 && i % 2 == 0 && j % 2 == 0)
+		sum = arr[i][j] + sum;
+
+}
+			
+
+
+		printf("%d\n", sum);
+	}
